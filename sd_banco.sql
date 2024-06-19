28a5c6e6353e7011c79e97ce9c658cb8dcd866e0
alex-wladyka <alexdiegowladyka@gmail.com>
2024-06-19 14:01:45 -0300

    Entrega 3 Final

---
+++ sd_banco.sql
@@ -0,0 +1,50 @@
+	CREATE DATABASE projeto_sd;
+    USE projeto_sd;
+    
+    CREATE TABLE IF NOT EXISTS candidate(
+    id int NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    Email varchar(100),
+    Nome varchar(60),
+    senha varchar(20)
+    );
+    
+    
+    CREATE TABLE IF NOT EXISTS recruiter(
+    id int NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    email varchar(100),
+    nome varchar(60),
+    senha varchar(20),
+    industry varchar(60),
+    description varchar(255)
+    );
+    
+    CREATE TABLE IF NOT EXISTS skillDataset(
+    idSkill int NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    nameSkill varchar(20)
+    );
+    
+    CREATE TABLE IF NOT EXISTS skills(
+    idCandidate int NOT NULL,
+    idSkillDataset int NOT NULL,
+    experiencia int NOT NULL,
+    FOREIGN KEY (idCandidate) REFERENCES candidate(id),
+    FOREIGN KEY (idSkillDataset) REFERENCES skillDataset(idSkill),
+    PRIMARY KEY(idCandidate, idSkillDataset)
+    );
+    
+    INSERT INTO skillDataset (nameSkill)
+    VALUES ('NodeJs'),('JavaScript'),('Java'),('C'),('HTML'),('CSS'),('React'),('ReactNative'),('TypeScript'),('Ruby');
+    
+    
+CREATE TABLE IF NOT EXISTS jobs(
+idJob int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+idRecruiter int NOT NULL,
+idSkillDataset int NOT NULL,
+experiencia int,
+FOREIGN KEY (idRecruiter) REFERENCES recruiter(id),
+FOREIGN KEY (idSkillDataset) REFERENCES skillDataset(idSkill)
+)
+    
+    
+    
+    
